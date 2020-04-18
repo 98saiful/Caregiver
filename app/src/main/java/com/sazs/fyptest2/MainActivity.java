939503036d965d829b3668a209293a7fbe41f6ab
Saiful Amir <saifulamir98@gmail.com>
2020-04-18 13:26:56 +0800

    Material Design implementation

---
+++ app/src/main/java/com/sazs/fyptest2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sazs.fyptest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
