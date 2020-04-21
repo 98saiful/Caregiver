94f51ccb9c29683efb95d5f392a560a8357e482a
Saiful Amir <saifulamir98@gmail.com>
2020-04-21 14:21:13 +0800

    Order Detail screen

--- app/src/main/java/com/sazs/fyptest2/OrderDetail.java
+++ app/src/main/java/com/sazs/fyptest2/OrderDetail.java
@@ -3,7 +3,9 @@ package com.sazs.fyptest2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderDetail extends AppCompatActivity {
 
@@ -16,5 +18,38 @@ public class OrderDetail extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getInfo();
+    }
+
+    private void getInfo(){
+        Intent intent = getIntent();
+        String name = intent.getExtras().getString("name");
+        Integer age = intent.getExtras().getInt("age");
+        String gender = intent.getExtras().getString("gender");
+        Integer height = intent.getExtras().getInt("height");
+        Integer weight = intent.getExtras().getInt("weight");
+        String location = intent.getExtras().getString("location");
+        String info = intent.getExtras().getString("info");
+        String note = intent.getExtras().getString("note");
+
+        TextView tvName = findViewById(R.id.name);
+        TextView tvAge = findViewById(R.id.age);
+        TextView tvGender = findViewById(R.id.gender);
+        TextView tvHeight = findViewById(R.id.height);
+        TextView tvWeight = findViewById(R.id.weight);
+        TextView tvLocation = findViewById(R.id.location);
+        TextView tvInfo = findViewById(R.id.info);
+        TextView tvNote = findViewById(R.id.note);
+
+        tvName.setText(name);
+        tvAge.setText((String.valueOf(age)));
+        tvGender.setText(gender);
+        tvHeight.setText((String.valueOf(height)));
+        tvWeight.setText((String.valueOf(weight)));
+        tvLocation.setText(location);
+        tvInfo.setText((info));
+        tvNote.setText(note);
+
     }
 }
--- app/src/main/java/com/sazs/fyptest2/adapter/OrderAdapter.java
+++ app/src/main/java/com/sazs/fyptest2/adapter/OrderAdapter.java
@@ -1,6 +1,7 @@
 package com.sazs.fyptest2.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.sazs.fyptest2.Order;
+import com.sazs.fyptest2.OrderDetail;
 import com.sazs.fyptest2.R;
 
 import java.util.List;
@@ -38,6 +40,25 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.OrderViewHol
         holder.tvElderlyName.setText(order.getElderly_name());
         holder.tvElderlyAge.setText((String.valueOf(order.getElderly_age())));
         holder.tvElderlyLocation.setText(order.getElderly_location());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(v.getContext(), OrderDetail.class);
+                Order order = orderList.get(position);
+
+                intent.putExtra("name", order.getElderly_name());
+                intent.putExtra("age", order.getElderly_age());
+                intent.putExtra("gender", order.getElderly_gender());
+                intent.putExtra("height", order.getElderly_height());
+                intent.putExtra("weight", order.getElderly_weight());
+                intent.putExtra("location", order.getElderly_location());
+                intent.putExtra("info", order.getElderly_info());
+                intent.putExtra("note", order.getElderly_note());
+
+                v.getContext().startActivity(intent);
+            }
+        });
     }
 
     @Override
